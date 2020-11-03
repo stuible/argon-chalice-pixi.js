10f71c32dcdcd6b1af2158617d4bf1e95a7c006c
Josh Stuible <joshstuible@gmail.com>
2020-11-03 02:17:51 -0800

    Style main menu, add description

--- src/components/MainMenu/index.vue
+++ src/components/MainMenu/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="main-menu">
     <div class="title">Macademia High</div>
+    <p class="tagline">Welcome to Macademia High, where things are absolutely nuts 🌰</p>
     <button @click="$store.commit('startGame')">Start Game</button>
   </div>
 </template>
@@ -12,10 +13,17 @@ export default {};
 <style lang="scss">
 #main-menu {
   height: 100%;
-  background-color: #2a3536;
+  background-color: #a6e9db;
+  padding: 4em;
 
   .title {
       font-size: 2em;
+      margin-bottom: 1em;
+  }
+
+  .tagline {
+      font-size: 1.15em;
+      margin-bottom: 5em;
   }
 
   button {
